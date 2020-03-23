411c13d349d9f10e6b56ab8f8c936833c75e24ad
Sumit Kumar <sumit09211@gmail.com>
2020-03-23 17:16:41 +0530

    Updates

---
+++ precache-manifest.fa24077dfe45a98285e17f2c55beee41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1eed9e476e4374f040f7846dca7b22",
+    "url": "/sumit09211/cra.git/index.html"
+  },
+  {
+    "revision": "81ae349c3f61881d5aae",
+    "url": "/sumit09211/cra.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c233afa18bafd0c2cb86",
+    "url": "/sumit09211/cra.git/static/js/2.b4ab6859.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sumit09211/cra.git/static/js/2.b4ab6859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ae349c3f61881d5aae",
+    "url": "/sumit09211/cra.git/static/js/main.1b6ab294.chunk.js"
+  },
+  {
+    "revision": "c7fb21e502af58b558fc",
+    "url": "/sumit09211/cra.git/static/js/runtime-main.85919f93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sumit09211/cra.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
